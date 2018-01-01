d0ee2bef65127f4357ef311c61f3fc373f6dbedf
xuan huy ngo <>
2018-01-01 16:53:28 +0700

    Recursive Backtracking (Easy)

---
+++ uva00624.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int const MAX_TRACKS = 25;
+int trackLength[MAX_TRACKS];
+int n, numOfTracks, i;
+int bitmask, total;
+
+void Choose(int currentTotal, int currentBitmask, int currentIndex)
+{
+	if (currentTotal > n)
+	{
+		return;
+	}
+	if (currentTotal > total) {
+		total = currentTotal;
+		bitmask = currentBitmask;
+	}
+	if (currentIndex >= numOfTracks)
+	{
+		return;
+	}
+	Choose(currentTotal + trackLength[currentIndex], currentBitmask | (1 << currentIndex), currentIndex + 1);
+	Choose(currentTotal, currentBitmask, currentIndex + 1);
+}
+
+int main()
+{
+	while (scanf_s("%d %d", &n, &numOfTracks) != EOF)
+	{
+		for (i = 0; i < numOfTracks; i++)
+		{
+			scanf_s("%d", &trackLength[i]);
+		}
+		total = -1, bitmask = -1;
+		Choose(0, 0, 0);
+
+		for (i = 0; i < numOfTracks; i++) {
+			if ((bitmask >> i) % 2)
+				printf_s("%d ", trackLength[i]);
+		}
+		printf_s("sum:%d\n", total);
+	}
+	return 0;
+}
